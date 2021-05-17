c4113b9ac7c8c2b8d50f8b12f1284efc03515f8a
egag8843 <egag8843@gmail.com>
2021-05-17 11:37:35 +0900

    2021/05/17 java Ch02 Example

---
+++ Ch02/src/sub2/VariableTest.java
@@ -0,0 +1,17 @@
+package sub2;
+/*
+
+ * */
+public class VariableTest {
+	public static void main(String[] args) {
+		
+		int num1;
+		num1 = 1;
+		
+		int num2 = 2;
+		
+		int num3 = num1+num2;
+		
+		System.out.println("num3 : "+num3);
+	}
+}
